a175ce211219b5503d4477ec2a007e81ecfc0145
Curtis Poe <ovid@cpan.org>
2010-01-31 19:09:19 +0000

    Update the docs.

--- lib/Test/Class/Most.pm
+++ lib/Test/Class/Most.pm
@@ -36,9 +36,84 @@ our $VERSION = '0.01';
     use Test::Class::Most parent => 'My::Test::Class';
 
     sub teststuff : Tests {
-        ok 1;
+        ok 1;  # All Test::Most functions exported
     }
 
+=head1 DESCRIPTION
+
+If you're not familiar with using L<Test::Class>, please see my tutorial at:
+
+=over 4
+
+=item * L<http://www.modernperlbooks.com/mt/2009/03/organizing-test-suites-with-testclass.html>
+
+=item * L<http://www.modernperlbooks.com/mt/2009/03/reusing-test-code-with-testclass.html>
+
+=item * L<http://www.modernperlbooks.com/mt/2009/03/making-your-testing-life-easier.html>
+
+=item * L<http://www.modernperlbooks.com/mt/2009/03/using-test-control-methods-with-testclass.html>
+
+=item * L<http://www.modernperlbooks.com/mt/2009/03/working-with-testclass-test-suites.html>
+
+=back
+
+When people write test classes with the excellent C<Test::Class>, you often
+see the following at the top of the code:
+
+  package Some::Test::Class;
+
+  use strict;
+  use warnings;
+  use base 'My::Test::Class';
+  use Test::More;
+  use Test::Exception;
+
+  # and then the tests ...
+
+That's a lot of boilerplate and I don't like boilerplate.  So now you can do
+this:
+
+  use Test::Class::Most parent => 'My::Test::Class';
+
+That automatically imports L<strict> and L<warnings> for you.  It also gives
+you all of the testing goodness from L<Test::Most>.  As an added bonus, if you
+have 5.10 or greater, you automatically get I<all> the new 5.10 features
+enabled, along with the c3 MRO.  Unless you don't want them:
+
+  use Test::Class::Most feature => 0;
+
+=head1 CREATING YOUR OWN BASE CLASS
+
+You probably want to create your own base class for testing.  To do this,
+simply specify no import list:
+
+  package My::Test::Class;
+  use Test::Class::Most; # we now inherit from Test::Class
+
+  INIT { Test::Class->runtests }
+
+  1;
+
+And then your other classes inherit as normal (well, the way we do it):
+
+  package Tests::For::Foo;
+  use Test::Class::Most parent => 'My::Test::Class';
+
+And you can inherit from those other classes, too:
+
+  package Tests::For::Foo::Child;
+  use Test::Class::Most parent => 'Tests::For::Foo';
+
+Of course, it's quite possible that you're a fan of multiple inheritance, so
+you can do that, too (I was I<soooooo> tempted to not allow this, but I
+figured I shouldn't force too many of my personal beliefs on you):
+
+ package Tests::For::ISuckAtOO;
+ use Test::Class::Most parent => [qw/
+    Tests::For::Foo
+    Tests::For::Bar
+    Some::Other::Class::For::Increased::Stupidity
+ /];
 
 =head1 EXPORT
 
@@ -123,9 +198,28 @@ L<http://search.cpan.org/dist/Test-Class-Most/>
 
 =back
 
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Test::Class>
+
+xUnit-style testing in Perl
+
+=item * L<Test::Most>
+
+The most popular CPAN test modules bundled into one module.
+
+=item * L<Modern::Perl>
+
+I stole this code.  Thanks C<chromatic>!
+
+=back
 
 =head1 ACKNOWLEDGEMENTS
 
+Thanks to Adrian Howard for L<Test::Class>, Adam Kennedy for maintaining it
+and C<chromatic> for L<Modern::Perl>.
 
 =head1 COPYRIGHT & LICENSE
 
@@ -134,7 +228,6 @@ Copyright 2010 Curtis "Ovid" Poe, all rights reserved.
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
 
-1;    # End of Test::Class::Most
+"Boilerplate it bad, m'kay";
